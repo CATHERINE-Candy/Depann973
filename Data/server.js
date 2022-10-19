5dc03cd580594acfa7e963acc67a59bf9d4e4625
ludoc0de <champi.ludo@tutanota.com>
2022-10-19 10:07:19 -0300

    add route

--- Data/server.js
+++ Data/server.js
@@ -1,7 +1,7 @@
 
 const express = require('express');
 const app = express();
-//const homeRoutes =require('./routes/home');
+const indexRoutes =require('./routes/index');
 require('dotenv').config({path: './config/.env'});
 const { request } = require('express');
 //const bodySanitizer = require('./app/middlewares/bodySanitizer');
@@ -11,6 +11,8 @@ app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }));
 //app.use(bodySanitizer);
 
+//Set Routes
+app.use('/', indexRoutes);
 
 //start server
 app.listen(process.env.PORT || PORT, ()=> console.log(`Server is running on ${process.env.PORT}!`)) 
