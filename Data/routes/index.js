5efee1622c0228504ce01276d4842198ded9c12f
ludoc0de <champi.ludo@tutanota.com>
2022-10-19 10:38:11 -0300

    update to get

--- Data/routes/index.js
+++ Data/routes/index.js
@@ -1,11 +1,10 @@
 //Index routes
-const express = require('express')
-const router = express.Router()
-const indexController = require('../controllers/index')
+const express = require('express');
+const router = express.Router();
+const indexController = require('../controllers/index');
 
 
 //Route to the controllers
-router.post('/', indexController.getIndex);
-
+router.get('/', indexController.getIndex);
 
 module.exports = router
